be3c04e43889b1eec643a8f34f11fdbd4a6fb768
Anton2405 <antonvershkov@mail.ru>
2023-12-04 22:02:15 +0300

    step5

--- my-app/src/components/AppRouter.jsx
+++ my-app/src/components/AppRouter.jsx
@@ -4,20 +4,35 @@ import About from '../pages/About'
 import Post from '../pages/Post'
 import Error from '../pages/Error'
 import PostIdPage from '../pages/PostIdPage'
-import { routes } from '../router/route'
+import { publicRoutes, privateRoutes } from '../router/route'
 export default function AppRouter() {
+    const isAuth = true;
     return (
-        <Routes>
-            {routes.map(route =>
-                <Route
-                    key={route.key}
-                    path={route.path}
-                    element={<route.element />}
-                    exact={route.exact}
-                />
-            )}
-        </Routes>
+
+        isAuth
+            ?
+            <Routes>
+                {privateRoutes.map(route =>
+                    <Route
+                        key={route.key}
+                        path={route.path}
+                        element={<route.element />}
+                        exact={route.exact}
+                    />
+                )}
+            </Routes>
+            :
+            <Routes>
+                {publicRoutes.map(route =>
+                    <Route
+                        key={route.key}
+                        path={route.path}
+                        element={<route.element />}
+                        exact={route.exact}
+                    />
+                )}
+            </Routes>
 
     )
 }
-//2:33
+//2:38
--- my-app/src/router/route.js
+++ my-app/src/router/route.js
@@ -2,9 +2,14 @@ import About from '../pages/About'
 import Post from '../pages/Post'
 import Error from '../pages/Error'
 import PostIdPage from '../pages/PostIdPage'
-export const routes = [
+import Login from '../pages/Login'
+export const privateRoutes = [
     { path: '/about', element: About, exact: true, key: 1 },
     { path: '/posts', element: Post, exact: true, key: 2 },
     { path: '/posts/:id', element: PostIdPage, exact: true, key: 3 },
     { path: '*', element: Post, exact: true, key: 4 }
+]
+export const publicRoutes = [
+    { path: '/login', element: Login, exact: true, key: 1 },
+    { path: '*', element: Login, exact: true, key: 2 }
 ]
